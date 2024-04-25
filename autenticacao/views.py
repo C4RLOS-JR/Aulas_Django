8551fb60c2fd92203ba9f6fa84dd83d61ba590f6
Leonam-Comercial <leonam.comseg@gmail.com>
2024-04-25 14:10:07 -0300

    Leonam

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
 
 def cadastro(request):
-  return render(request, 'cadastro/index.html')
+  if request.method == 'GET':
+    return render(request, 'cadastro/index.html')
+  elif request.method == 'POST':
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    return HttpResponse(json.dumps({'nome': nome, 'email': email}))
