a0ebd80a21b6c9de4e5f74b784871afc2d56f38e
Leonam-Comercial <leonam.comseg@gmail.com>
2024-04-30 16:37:19 -0300

    Leonam

--- autenticacao/urls.py
+++ autenticacao/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('cadastro/', views.cadastro, name = 'cadastro'),
-    path('listar', views.listar, name = 'listar'),
+    path('listar/', views.listar, name = 'listar'),
+    path('alterar/<int:id>', views.alterar, name = 'alterar'),
+    path('excluir/<int:id>', views.excluir, name = 'excluir'),
+
 ]
--- autenticacao/views.py
+++ autenticacao/views.py
@@ -20,12 +20,40 @@ def cadastro(request):
   
 def listar(request):
   pessoas = Pessoa.objects.all()
-  nome = request.POST.get('nome')
-  email = request.POST.get('email')
-  print(nome)
-  print(email)
 
-  # if not nome and not email:
-  #   return render(request, 'listar/index.html', {'pessoas': pessoas})
-  # else:
-  return render(request, 'listar/index.html')
+  if request.method == 'GET':
+    return render(request, 'listar/listar.html')
+  elif request.method == 'POST':
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+
+    if not nome and not email:
+      return render(request, 'listar/listar.html', {'pessoas': pessoas})
+    else:
+      pessoa = Pessoa.objects.filter(nome=nome) | Pessoa.objects.filter(email=email)
+      return render(request, 'listar/listar.html', {'pessoa': pessoa})
+    
+
+def alterar(request, id):
+  if request.method == 'GET':
+    return render(request, 'listar/alterar.html', {'id': id})
+  if request.method == 'POST':
+    nome = request.POST.get('nome')
+    email = request.POST.get('email')
+    senha = request.POST.get('senha')
+    pessoa_alterar = Pessoa.objects.filter(id=id)[0]  # na posição '0', primeira e única posição da lista.
+
+    pessoa_alterar.nome = nome
+    pessoa_alterar.email = email
+    pessoa_alterar.senha = senha
+
+    pessoa_alterar.save()
+    
+    return render(request, 'listar/listar.html')
+  
+
+def excluir(request, id):
+  pessoa_excluir = Pessoa.objects.filter(id=id)
+  pessoa_excluir.delete()
+  
+  return render(request, 'listar/listar.html')
