335c2be9f014a6aea248d0ea876888e590282590
C4RLOS-JR <camjr3z@gmail.com>
2024-05-01 15:23:14 -0300

    Carlos

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
-from .models import Pessoa
-import json
+from .models import Cargos, Pessoa
 
 def cadastro(request):
   if request.method == 'GET':
@@ -10,29 +9,34 @@ def cadastro(request):
     nome = request.POST.get('nome')
     email = request.POST.get('email')
     senha = request.POST.get('senha')
+    cargo = request.POST.get('cargo')
+    cargo = Cargos.objects.get(id=cargo)
+
     pessoa = Pessoa(nome=nome, 
                     email=email,
-                    senha=senha)
+                    senha=senha,
+                    cargo=cargo)
     pessoa.save()
 
-    return redirect('/autenticacao/cadastro')
+    return render(request, 'cadastro/index.html', {'status': '0'})
   
   
 def listar(request):
-  pessoas = Pessoa.objects.all()
-
   if request.method == 'GET':
     return render(request, 'listar/listar.html')
   elif request.method == 'POST':
     nome = request.POST.get('nome')
     email = request.POST.get('email')
 
-    if not nome and not email:
-      return render(request, 'listar/listar.html', {'pessoas': pessoas})
+    if nome or email:
+      if nome and email:
+        pessoas = Pessoa.objects.filter(nome=nome).filter(email=email)
+      else:
+        pessoas = Pessoa.objects.filter(nome=nome) | Pessoa.objects.filter(email=email).all()
     else:
-      pessoa = Pessoa.objects.filter(nome=nome) | Pessoa.objects.filter(email=email)
-      return render(request, 'listar/listar.html', {'pessoa': pessoa})
+      pessoas = Pessoa.objects.all()
     
+    return render(request, 'listar/listar.html', {'pessoas': pessoas})
 
 def alterar(request, id):
   if request.method == 'GET':
@@ -41,19 +45,26 @@ def alterar(request, id):
     nome = request.POST.get('nome')
     email = request.POST.get('email')
     senha = request.POST.get('senha')
-    pessoa_alterar = Pessoa.objects.filter(id=id)[0]  # na posição '0', primeira e única posição da lista.
+    cargo = request.POST.get('cargo')
+    cargo = Cargos.objects.get(id=cargo)
+    # pessoa_alterar = Pessoa.objects.filter(id=id)[0]  # na posição '0', primeira e única posição da lista.
+    pessoa_alterar = Pessoa.objects.get(id=id)
 
-    pessoa_alterar.nome = nome
-    pessoa_alterar.email = email
-    pessoa_alterar.senha = senha
-
+    if nome:
+      pessoa_alterar.nome = nome
+    if email:
+      pessoa_alterar.email = email
+    if senha:
+      pessoa_alterar.senha = senha
+    if cargo:
+      pessoa_alterar.cargo = cargo
     pessoa_alterar.save()
     
-    return render(request, 'listar/listar.html')
+    return render(request, 'listar/listar.html', {'status': '1'})
   
 
 def excluir(request, id):
   pessoa_excluir = Pessoa.objects.filter(id=id)
   pessoa_excluir.delete()
   
-  return render(request, 'listar/listar.html')
+  return render(request, 'listar/listar.html', {'status': '2'})
